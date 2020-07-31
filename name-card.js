cf451bf030b575dc8b207157dbe7725ab6283744
alanderson96 <alxanderson96@gmail.com>
2020-07-31 15:09:06 -0600

    figured out how to create a display name

--- name-card.js
+++ name-card.js
@@ -1,3 +1,23 @@
-console.log("Hello World!")
+console.log("Hello World!");
 
 //create a display name
+let myName = "";
+const ourInput = document.getElementById("displayNameInput");
+
+ourInput.addEventListener("keypress", function (e) {
+  console.log("ekey", e.key);
+  if (e.key !== "Enter") {
+    myName = myName + e.key;
+  }
+
+  if (e.key === "Enter") {
+    // console.log("displayName");
+
+    // const displayName = document.getElementById("displayNameInput").innerText;
+    // console.log("displayName", displayName);
+    document.getElementById("displayName").innerHTML = myName;
+    document.getElementById("displayNameInput").value = "";
+    myName = "";
+    e.preventDefault();
+  }
+});
